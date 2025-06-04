7a2d08409e909c1813b5ba71ebae34eb52e5f77f
Developer1 <vbathala@asu.edu>
2025-06-04 12:57:45 -0500

    updated variable.tf

--- variables.tf
+++ variables.tf
@@ -10,4 +10,10 @@ variable "subnetids" {
     "subnet-05bb9c5953b444440",
     "subnet-0f1e2081eb1b9dff8"
   ]
-}  
+}
+
+variable "region" {
+  description = "AWS region where the EKS cluster is deployed"
+  type        = string
+  default     = "us-east-1"
+}
